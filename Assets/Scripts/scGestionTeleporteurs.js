514bef898eb2aa4fc97076a0ff1f81bea99a3650
Alex BOUET <alexbouet.ca@gmail.com>
2016-02-05 20:41:12 -0500

    Revert "Ajout de Kaseem, dialogues, téléportation"

--- Assets/Scripts/scGestionTeleporteurs.js
+++ Assets/Scripts/scGestionTeleporteurs.js
@@ -25,43 +25,6 @@ function OnTriggerEnter(trigg:Collider)
 		Application.LoadLevel (1);
 	}
 
-	if (trigg.gameObject.tag == "teleportNiveau4") 
-	{
-		Application.LoadLevel (5);
-	}
-
-	if (trigg.gameObject.tag == "teleportNiveau5") 
-	{
-		Application.LoadLevel (6);
-	}
-
-
-
-	//Téléportation du personnage d'une étage à l'autre selon les niveaux
-
-
-	//Niveau 4
-
-		if (trigg.gameObject.tag == "teleportNiveau4(2Etage)") 
-	{
-		this.transform.position = new Vector3(69,2,156);
-	}
-
-		if (trigg.gameObject.tag == "teleportNiveau4(1Etage)") 
-	{
-		this.transform.position = new Vector3(136,2,160);
-
-	}
-
-
-
-	//Niveau 2
-
-	    if (trigg.gameObject.tag == "teleportNiveau2(2Etage)") 
-    {
-         this.transform.position = Vector3(-94.36, 1, 237.96);
-    }
-
 
 }
 
